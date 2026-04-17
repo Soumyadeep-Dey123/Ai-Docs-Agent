842b689c665a40228441257fef502e1848534336
SoumyadeepDey <dey.soumyadeep123@gmail.com>
2026-04-17 21:12:16 +0530

    Initial commit. Bare bones Project

---
+++ README.md
@@ -0,0 +1,14 @@
+# AI Docs Agent
+
+Started: 2026-04-17 | Deadline: 2026-05-01
+Goal: Build a project that answers questions about a certain framework(for now sticking to laravel) from their official docs official docs
+
+## Progress
+
+- [x] Stage 1 — Fetch a simple response from gemini api
+- [ ] Stage 2 — Creation of system prompt, structured answer display
+- [ ] Stage 3 — Feed extra data(manually) and display a structured response
+- [ ] Stage 4 — Use a webscraper to fetch data and feed it along with the response
+- [ ] Stage 5 — Make it agentic. Add thinking and add a way to store the fetched data(in a db)
+- [ ] Stage 6 — Work on memory, make the data available for later uses
+- [ ] Stage 7 — Improve responses, make it produce and retain more info
---
+++ main.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# api_key = os.getenv("API_KEY")
+def main():
+    # Initialize the client
+    client = OpenAI(
+        api_key=os.getenv("GEMINI_API_KEY"),
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+
+    response = client.chat.completions.create(
+        model="gemini-3-flash-preview",
+        messages=[
+            {   "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "Explain to me how AI works"
+            }
+        ]
+    )
+
+    print(response.choices[0].message)
+
+
+if __name__ == "__main__":
+    main()
